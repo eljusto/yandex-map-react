45a12ef063df6abdeaeffe482b3a43f5d175371c
Boris Zhidkov <eljusto@yandex-team.ru>
2016-11-15 12:44:00 +0300

    added polyfill for Promise due to it's unsupported in IE

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import Map from './MapContainer';
 import Marker from './MapMarker';
 import MarkerLayout from './MarkerLayout';
 import BalloonLayout from './BalloonLayout';
-import ConstructorJSONImport from './ConstructorJSONImport'
+import ConstructorJSONImport from './ConstructorJSONImport';
+
+// auto-polyfill Promise
+import 'es6-promise/auto';
 
 export {
     Map,
